c4e6b4cb4e7f2439154389773da47c8f125b23fa
Linkmidna <juliofrancisco.higueracolino@iesmiguelherrero.com>
2023-04-18 17:30:32 +0200

    Empezando UniCinema

---
+++ UniCine_JulioF/UniCine_JulioF/ContenedorMDI.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace UniCine_JulioF
+{
+    partial class ContenedorMDI
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.mantenimientosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.peliculasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sesionesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.proyeccionesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mantenimientosToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // mantenimientosToolStripMenuItem
+            // 
+            this.mantenimientosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.peliculasToolStripMenuItem,
+            this.sesionesToolStripMenuItem,
+            this.proyeccionesToolStripMenuItem});
+            this.mantenimientosToolStripMenuItem.Name = "mantenimientosToolStripMenuItem";
+            this.mantenimientosToolStripMenuItem.Size = new System.Drawing.Size(106, 20);
+            this.mantenimientosToolStripMenuItem.Text = "Mantenimientos";
+            // 
+            // peliculasToolStripMenuItem
+            // 
+            this.peliculasToolStripMenuItem.Name = "peliculasToolStripMenuItem";
+            this.peliculasToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.peliculasToolStripMenuItem.Text = "Peliculas";
+            // 
+            // sesionesToolStripMenuItem
+            // 
+            this.sesionesToolStripMenuItem.Name = "sesionesToolStripMenuItem";
+            this.sesionesToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.sesionesToolStripMenuItem.Text = "Sesiones";
+            // 
+            // proyeccionesToolStripMenuItem
+            // 
+            this.proyeccionesToolStripMenuItem.Name = "proyeccionesToolStripMenuItem";
+            this.proyeccionesToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.proyeccionesToolStripMenuItem.Text = "Proyecciones";
+            // 
+            // ContenedorMDI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "ContenedorMDI";
+            this.Text = "Universal Cinema";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem mantenimientosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem peliculasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sesionesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem proyeccionesToolStripMenuItem;
+    }
+}
+
---
+++ UniCine_JulioF/UniCine_JulioF/Negocio.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniCine_JulioF
+{
+
+    public class Negocio
+    {
+        private static UniCine bd;
+        static Negocio()
+        {
+            bd = new UniCine();
+        }
+    }
+}
